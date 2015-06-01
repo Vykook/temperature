aa3872fecfa89027d61e4ee3557d594bdcc67719
Vykook <vykook@icloud.com>
2015-06-01 20:53:26 +0200

    fixed db query. search limited to last 24 hours.

--- gui/server.js
+++ gui/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongo = require('mongodb').MongoClient, format = require('util').format;
+var moment = require('moment');
 
 var app = express();
 
@@ -8,10 +9,15 @@ app.set('view engine', 'jade');
 app.use("/bower_components", express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res) {
+	var now = moment();
+	var t24 = now.subtract(1, 'days').toISOString();
+
 	mongo.connect('mongodb://mymongo:27017/home', function(err, db) {
 		if(err) throw err;
+
 		var collection = db.collection('temperature');
-		collection.find({}, {_id: 0, temperature: 1, humidity: 1, date: 1}).sort({date: 1}).limit(1000).toArray(function(err, results) {
+
+		collection.find({date:{'$gte': new Date(t24)}}, {_id: 0, temperature: 1, humidity: 1, date: 1}).sort({date: 1}).limit(1000).toArray(function(err, results) {
 			res.render('index',
 				{data: results}
 			);
